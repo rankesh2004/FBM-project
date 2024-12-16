97b166c48927c083c4a85625eabfde1208154b41
rankesh2004 <rangesh.csr.2004@gmail.com>
2024-12-16 19:00:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlightComponent } from './flight/flight.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FlightComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
